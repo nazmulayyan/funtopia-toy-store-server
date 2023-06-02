435fe421891c14b29368c3bf82cd46b1c1a8a24b
NazmulAyyan <rnazmul2424@gmail.com>
2023-06-02 22:12:33 +0600

    new toy added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
-console.log(process.env.DB_PASS);
 
 /*-----------
     mongodb
@@ -30,12 +29,54 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+   
+
+    //add a new toy
+    const addToyCollection = client.db('funtopiaToys').collection('toys');
+
+    app.get('/addToy', async(req, res) =>{
+        const cursor = addToyCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // app.get('/addToy/:id', async (req,res) =>{
+    //     const id =  req.params.id;
+    //     const query = {_id: new ObjectId (id)};
+    //     const toyDetails = await addToyCollection.findOne(query);
+    //     res.send(toyDetails)
+    // })
+
+    app.post('/addToy', async(req, res)=>{
+        const addNewToy = req.body;
+        console.log(addNewToy);
+        const result = await addToyCollection.insertOne(addNewToy);
+        res.send(result);
+    })
+
+    //blogs data fetch
     const blogCollection = client.db('funtopiaToys').collection('blogs');
 
     app.get('/blogs', async(req, res)=>{
         const cursor = blogCollection.find();
         const result = await cursor.toArray();
         res.send(result);
+    })
+
+    // category data fetch
+    const toyCollection = client.db('funtopiaToys').collection('toys');
+
+    app.get('/toys', async(req, res)=>{
+        const cursor = toyCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/toys/:id', async (req,res) =>{
+        const id =  req.params.id;
+        const query = {_id: new ObjectId (id)};
+        const toyDetails = await toyCollection.findOne(query);
+        res.send(toyDetails)
     })
 
     // Send a ping to confirm a successful connection
