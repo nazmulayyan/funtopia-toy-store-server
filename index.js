9b95d9bacf875177c8c8a698e5dc8791b1534e8d
NazmulAyyan <rnazmul2424@gmail.com>
2023-06-12 23:43:32 +0600

    checked the code before deploy all are okay

--- index.js
+++ index.js
@@ -85,7 +85,6 @@ async function run() {
             res.send(result);
         })
 
-
         //my toys
         app.get('/myToy/:email', async (req, res) => {
             const email = req.params?.email
@@ -161,7 +160,8 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
+    }
+    finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
